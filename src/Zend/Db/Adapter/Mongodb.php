5df586ea1b8a48d5e43cbf288bb3b8e4a5b1a8ca
Jakub Frieb <friebj@magexo.cz>
2017-09-10 18:15:29 +0200

    fix issue with access to particular db

--- src/Zend/Db/Adapter/Mongodb.php
+++ src/Zend/Db/Adapter/Mongodb.php
@@ -64,7 +64,7 @@ class Zend_Db_Adapter_Mongodb extends Zend_Db_Adapter_Abstract
         unset($this->_options['host'], $this->_options['port'], $this->_options['dbname']);
 
         $this->_connection = new $this->_serverClass(
-            'mongodb://' . $config['host'] . ':' . $config['port'],
+            'mongodb://' . $config['host'] . ':' . $config['port'].'/'.$config['db'],
             $this->_options
         );
 
